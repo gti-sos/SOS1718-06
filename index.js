40a8bb1b17adff2b26146c1302306a2248d7e194
sos171806aom <alvortmed@alum.us.es>
2018-03-02 15:08:48 +0000

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var cool = require("cool-ascii-faces");
 console.log(cool());
+
+var express = require("express");
+var app = express();
+app.get("/hello", (req,res)=>{
+   res.send("Hello World"); 
+});
+
+app.listen(process.env.PORT);
