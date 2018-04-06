6729719a0163bc94a35f2dae2dc136d50c7260fb
sos171806dgc <dionigc96@gmail.com>
2018-04-06 16:55:23 +0000

    cambios mayores

---
+++ generalBudgetsApi/index.js
@@ -0,0 +1,194 @@
+var generalBudgetsApi = {};
+var BASE_API_PATH = "/api/v1"; //variable global
+var BASE_API_PATH_GB = "/api/v1/generalBudgets";
+
+module.exports = generalBudgetsApi; //voy a exportar ese objeto
+
+generalBudgetsApi.register = function(app, db) {
+    console.log("Registering routs for contact API..");
+
+    app.get(BASE_API_PATH + "/help", (req, res) => {
+        res.redirect(""); //postman
+    });
+
+    var InitialGeneralBudgets = [{
+            "community": "andalucia",
+            "year": 2017,
+            "section": "parlamento-de-andalucia",
+            "total-budget": "45.479.748",
+            "percentage-over-total": "0.137"
+        },
+        {
+            "community": "andalucia",
+            "year": 2017,
+            "section": "deuda-publica",
+            "total-budget": "4.162.050.097",
+            "percentage-over-total": "12.521"
+        },
+        {
+            "community": "andalucia",
+            "year": 2017,
+            "section": "camara-de-cuentas",
+            "total-budget": "10.408.549",
+            "percentage-over-total": "31"
+        },
+        {
+            "community": "andalucia",
+            "year": 2017,
+            "section": "consejeria-de-educacion",
+            "total-budget": "6.100.323.433",
+            "percentage-over-total": "18.353"
+        },
+        {
+            "community": "andalucia",
+            "year": 2017,
+            "section": "consejeria-de-salud",
+            "total-budget": "725.885.406",
+            "percentage-over-total": "2.184"
+        },
+
+
+    ];
+
+    //loadInitialData
+    app.get(BASE_API_PATH_GB + "/loadInitialData", (req, res) => {
+        console.log(Date() + " - GET /loadInitialData" + InitialGeneralBudgets);
+
+        db.find({}).toArray((err, generalBudgets) => {
+            if (err) {
+                console.log("Error acccesing DB");
+                process.exit(1);
+                return;
+            }
+            if (generalBudgets.length == 0) {
+                console.log("Empty DB");
+                db.insert(InitialGeneralBudgets);
+            }
+            res.send(generalBudgets.map((c) => {
+                delete c._id;
+                return c;
+            }));
+        });
+
+    });
+
+    //-------GET a todos los presupuestos generales-------------
+    app.get(BASE_API_PATH + "/generalBudgets", (req, res) => {
+        console.log(Date() + " - GET /generalBudgets");
+
+        db.find({}).toArray((err, generalBudgets) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+
+            res.send(generalBudgets.map((c) => {
+                delete c._id;
+                return c;
+            }));
+        });
+
+    });
+
+    //--------GET de un solo recurso--------------
+    app.get(BASE_API_PATH + "/generalBudgets/:section", (req, res) => {
+        var section = req.params.section;
+        console.log(Date() + " - GET /section/" + section);
+
+        db.find({ "section": section }).toArray((err, generalBudgets) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+
+            res.send(generalBudgets.map((c) => {
+                delete c._id;
+                return c;
+            })[0]); // coge el primero que haya que cumpla la condición
+        });
+    });
+
+    //DELETE de TODO
+    app.delete(BASE_API_PATH + "/generalBudgets", (req, res) => { //igual
+        console.log(Date() + " - DELETE /generalBudgets");
+        generalBudgets = [];
+
+        db.remove({});
+
+        res.sendStatus(200);
+    });
+
+    //DELETE de algo concreto
+    app.delete(BASE_API_PATH_GB + "/:section", (req, res) => {
+        var section = req.params.section;
+        console.log(Date() + " - DELETE /generalBudgets/" + section);
+
+        db.remove({ "section": section });
+        res.sendStatus(200);
+    });
+
+    //POST normal
+    app.post(BASE_API_PATH_GB, (req, res) => {
+        console.log(Date() + " - POST /generalBudgets");
+        var budget = req.body;
+
+        if (!budget) {
+            console.log("warning : new Get req");
+            res.sendStatus(400);
+        }
+        db.find({ "section": budget.section }).toArray((err, generalBudgets) => {
+            if (err) {
+                console.log("error accesing db");
+                res.sendStatus(400);
+            }
+            if (generalBudgets.length > 0) {
+                console.log("warning");
+                res.sendStatus(409);
+            }
+            else {
+                db.insert(budget);
+                res.sendStatus(201);
+            }
+        });
+
+    });
+
+    //POST a un recurso concreto
+    app.post(BASE_API_PATH_GB + "/:section", (req, res) => { //debe dar error de método no permitido
+        var section = req.params.section;
+        console.log(Date() + " - POST /generalBudgets/" + section);
+        res.sendStatus(405);
+    });
+
+    //PUT general
+    app.put(BASE_API_PATH_GB, (req, res) => { // debe dar un error de método no permitido
+        console.log(Date() + " - PUT /generalBudgets");
+        res.sendStatus(405);
+    });
+
+    //PUT a un recurso concreto
+    /* app.put(BASE_API_PATH_GB+"/:section",(req,res)=>{
+    var section = req.params.section;
+    var b = req.body;
+    
+    console.log(Date() + " - PUT /generalBudgets/"+section);
+    
+   if (section != b.section) {
+        res.sendStatus(409);
+        console.warn(Date() + " - Hacking attempt!");
+        return;
+    }
+    
+    db.update({"section": b.section},b,(err,numUpdated)=>{
+        if (err) {
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+            return;
+        }
+        console.log("Updated: " + numUpdated);
+    });
+    res.sendStatus(200);
+});*/
+};
--- index.js
+++ index.js
@@ -5,112 +5,166 @@ var bodyParser = require("body-parser");
 var MongoClient = require("mongodb").MongoClient;
 
 var budgetsLawsApi = require("./budgetsLawsApi");
+var generalBudgetsApi = require("./generalBudgetsApi");
 
 var port = (process.env.PORT || 1607);
 
 var mdbURL = "mongodb://dbaom:sos1718-06@ds231559.mlab.com:31559/sos1718-aom-sandbox";
+var mdbGeneralBudgetURL = "mongodb://diogalcam:061196dioni@ds237489.mlab.com:37489/diogalcam";
 
 var app = express();
-app.use("/",express.static(__dirname+"/public"));
+app.use("/", express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-
-    
-var InitialBudgetsLaws = [
-        {
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "Agencia-publica-empresarial-de-la-radio-y-television-de-andalucia-RTVA-consolidado",
-            "budget-of-capital" : "5.686.779",
-            "total" : "169.692.400"
-        },
-         {
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "Agencia-publica-empresarial-de-la-radio-y-television-de-andalucia-RTVA",
-            "budget-of-capital" : "968.284",
-            "total" : "161.435.032"
-        },
-         {
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "Agencia-andaluza-del-conocimiento",
-            "budget-of-capital" : "200.000",
-            "total" : "7.153.248"
-        },
-         {
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "Agencia-publica-andaluza-de-educacion",
-            "budget-of-capital" : "1.500.000",
-            "total" : "379.706.833"
-        },
-         {
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "Agencia-publica-empresarial-sanitaria-bajo-Guadalquivir",
-            "budget-of-capital" : "800.000",
-            "total" : "50.892.169"
-        }
+var InitialGeneralBudgets = [{
+        "community": "andalucia",
+        "year": 2017,
+        "section": "parlamento-de-andalucia",
+        "total-budget": "45.479.748",
+        "percentage-over-total": "0.137"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "deuda-publica",
+        "total-budget": "4.162.050.097",
+        "percentage-over-total": "12.521"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "camara-de-cuentas",
+        "total-budget": "10.408.549",
+        "percentage-over-total": "31"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "consejeria-de-educacion",
+        "total-budget": "6.100.323.433",
+        "percentage-over-total": "18.353"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "consejeria-de-salud",
+        "total-budget": "725.885.406",
+        "percentage-over-total": "2.184"
+    },
 
 
-    ];
-    
+];
 
-MongoClient.connect(mdbURL,{native_parser:true},(err,mlabs)=>{
-   if(err){
-       console.error("Error accesing DB:"+ err);
-       process.exit(1);
-   }
-       console.log("Connected to DB in mlabs");
-       
-       var database = mlabs.db("sos1718-aom-sandbox");
-       var db = database.collection("budgetsLaws");
-   
-   
-   db.find({},(err,budgetsLaws)=>{
-    if(err){
-        console.error("error accesing db");
+var InitialBudgetsLaws = [{
+        "community": "andalucia",
+        "year": 2017,
+        "section": "Agencia-publica-empresarial-de-la-radio-y-television-de-andalucia-RTVA-consolidado",
+        "budget-of-capital": "5.686.779",
+        "total": "169.692.400"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "Agencia-publica-empresarial-de-la-radio-y-television-de-andalucia-RTVA",
+        "budget-of-capital": "968.284",
+        "total": "161.435.032"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "Agencia-andaluza-del-conocimiento",
+        "budget-of-capital": "200.000",
+        "total": "7.153.248"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "Agencia-publica-andaluza-de-educacion",
+        "budget-of-capital": "1.500.000",
+        "total": "379.706.833"
+    },
+    {
+        "community": "andalucia",
+        "year": 2017,
+        "section": "Agencia-publica-empresarial-sanitaria-bajo-Guadalquivir",
+        "budget-of-capital": "800.000",
+        "total": "50.892.169"
+    }
+
+
+];
+
+//----------------conexion DB Alvaro-------------------------
+MongoClient.connect(mdbURL, { native_parser: true }, (err, mlabs) => {
+    if (err) {
+        console.error("Error accesing DB:" + err);
         process.exit(1);
     }
-    if(InitialBudgetsLaws.length == 0){
-        console.log("Empty DB");
-        db.insert(InitialBudgetsLaws);
-    }else{
-        console.log("Db has " + InitialBudgetsLaws.length + " budgetsLaws");
-    }
+    console.log("Connected to DB in mlabs");
+
+    var database = mlabs.db("sos1718-aom-sandbox");
+    var db = database.collection("budgetsLaws");
+
+
+    db.find({}, (err, budgetsLaws) => {
+        if (err) {
+            console.error("error accesing db");
+            process.exit(1);
+        }
+        if (InitialBudgetsLaws.length == 0) {
+            console.log("Empty DB");
+            db.insert(InitialBudgetsLaws);
+        }
+        else {
+            console.log("Db has " + InitialBudgetsLaws.length + " budgetsLaws");
+        }
+    });
+
+
+    budgetsLawsApi.register(app, db);
+
+    //---------------------conexión BD DIONI--------------------------
+
+    MongoClient.connect(mdbGeneralBudgetURL, { native_parser: true }, (err, mlabs) => {
+        if (err) {
+            console.error("Error accesing DB:" + err);
+            process.exit(1);
+        }
+        console.log("\r\r\r\r"); //saltos de línea
+        console.log("Connected to DB in mlabs");
+
+        var database = mlabs.db("diogalcam");
+        var db = database.collection("generalBudgets");
+
+
+        db.find({}, (err, generalBudgets) => {
+            if (err) {
+                console.error("error accesing db");
+                process.exit(1);
+            }
+            if (InitialGeneralBudgets.length == 0) {
+                console.log("Empty DB");
+                db.insert(InitialGeneralBudgets);
+            }
+            else {
+                console.log("Db has " + InitialGeneralBudgets.length + " generalBudgets");
+            }
+        });
+
+
+
+
+
+        generalBudgetsApi.register(app, db);
+
+
+        app.listen(port, () => {
+            console.log("Server ready on port " + port + "!");
+        }).on("error", (e) => {
+            console.log("Server NOT READY:" + e);
+        });
+
+        console.log("Server setting up...");
+
+    });
 });
-
-budgetsLawsApi.register(app,db);
-
-app.listen(port,()=>{
-    console.log("Server ready on port" + port + "!");
-}).on("error",(e)=>{
-    console.log("Server not ready" + e);
-});
-       
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
