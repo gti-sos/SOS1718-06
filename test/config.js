4a3fb7062a518a17317dc4fa5259e0d664267dd3
sos171806zgg <zoigomgor@alum.us.es>
2018-05-27 21:32:14 +0000

    subiendo

--- test/config.js
+++ test/config.js
@@ -2,7 +2,7 @@ exports.config = {
     
     seleniumAddress: 'http://localhost:8910',  //en qué sitio hay corriendo un proceso que entienda el protocolo de selenium.
     
-    specs: ['T00-API','T01-loadData.js','T02-addSP.js'],  //qué archivos de tests voy a lanzar.
+    specs: ['T00-API.js','T01-loadData.js','T02-addSP.js'],  //qué archivos de tests voy a lanzar.
     
     capabilities: {
         'browserName': 'phantomjs'  //qué tipo de navegador es (phantomjs).
